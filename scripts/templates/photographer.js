6f9daea44e7d581959a3415a70182078925157d8
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-06-28 16:45:05 +0200

    Index elements with JS and CSS

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,42 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const figure = document.createElement('figure');
+
+        const figureMain = document.createElement('div');
+        figureMain.setAttribute("class", "figure_main");
+
+        const figureContent = document.createElement('div');
+        figureContent.setAttribute("class", "figure_content");
+
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+
+        const h3 = document.createElement('h3');
+        h3.textContent = city + ', ' + country;
+
+        const taglineElement = document.createElement('p');
+        taglineElement.textContent = tagline;
+
+        const priceElement = document.createElement('p');
+        priceElement.setAttribute("class", "figure_content_price");
+        priceElement.textContent = price + '€/jour';
+
+        figure.appendChild(figureMain);
+        figure.appendChild(figureContent);
+        figureMain.appendChild(img);
+        figureMain.appendChild(h2);
+        figureContent.appendChild(h3);
+        figureContent.appendChild(taglineElement);
+        figureContent.appendChild(priceElement);
+
+        return (figure);
     }
     return { name, picture, getUserCardDOM }
 }
