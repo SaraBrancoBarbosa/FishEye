6504bc9a430662727b7cfc28113d93a18a7b094b
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-07-17 17:16:18 +0200

    DOM new elements

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,46 +1,44 @@
-//export async function lightboxModalElements () {
-    /*********** Elements ***********/
+/*********** Elements ***********/
 
-    // DOM elements
-    const lightboxModal = document.querySelector(".lightbox_modal");
-    const backgroundHtml = document.body;
-    const btnCloselightboxModal = document.querySelector(".close_media_button");
-    const btnPrev = document.querySelector(".btn-prev");
-    const btnNext = document.querySelector("btn-next");
+// DOM elements
+const lightboxModal = document.querySelector(".lightbox_modal");
+const backgroundHtml = document.body;
+const btnCloselightboxModal = document.querySelector(".close_media_button");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+const mediaImg = document.querySelector(".medium-card_img");
+const mediaVideo = document.querySelector(".medium-card_video");
 
-    /*********** Launching and closing lightbox ***********/
+/*********** Launching and closing lightbox ***********/
 
-    // Function to prevent further propagation of the current event
-    const stopMediaPropagation = (event) => { 
-        event.stopPropagation();
+// Function to prevent further propagation of the current event
+const stopMediaPropagation = (event) => { 
+    event.stopPropagation();
+}
+
+// Cette fonction est intégrée dans pages/photographer.js
+export function launchLightboxModal() {
+    lightboxModal.style.display = "flex";
+    backgroundHtml.style.overflow = "hidden"; 
+    backgroundHtml.setAttribute = ("aria-hidden", true);
+    lightboxModal.addEventListener("click", closeLightboxModal); 
+    lightboxModal.querySelector(".modal").addEventListener("click", stopMediaPropagation);}
+
+function closeLightboxModal() {
+    lightboxModal.style.display = "none";
+    backgroundHtml.style.overflow = "auto";
+    backgroundHtml.setAttribute = ("aria-hidden", false);
+    lightboxModal.removeEventListener("click", closeLightboxModal);
+    lightboxModal.querySelector(".modal").removeEventListener("click", stopMediaPropagation);
+}
+
+// Closes modal by pressing the escape key
+window.addEventListener("keydown",(event) => {
+    if (event.key === 'Escape') {
+        closeLightboxModal()
     }
+});
 
-    // Cette fonction est intégrée dans pages/photographer.js
-    export function launchLightboxModal() {
-        lightboxModal.style.display = "flex";
-        backgroundHtml.style.overflow = "hidden"; 
-        backgroundHtml.setAttribute = ("aria-hidden", true);
-        lightboxModal.addEventListener("click", closeLightboxModal); 
-        lightboxModal.querySelector(".modal").addEventListener("click", stopMediaPropagation);}
+btnCloselightboxModal.addEventListener("click", closeLightboxModal);
 
-    function closeLightboxModal() {
-        lightboxModal.style.display = "none";
-        backgroundHtml.style.overflow = "auto";
-        backgroundHtml.setAttribute = ("aria-hidden", false);
-        lightboxModal.removeEventListener("click", closeLightboxModal);
-        lightboxModal.querySelector(".modal").removeEventListener("click", stopMediaPropagation);
-    }
-
-    // Closes modal by pressing the escape key
-    window.addEventListener("keydown",(event) => {
-        if (event.key === 'Escape') {
-            closeLightboxModal()
-        }
-    });
-
-    btnCloselightboxModal.addEventListener("click", closeLightboxModal);
-
-    //figureOpenModal.addEventListener("click", launchLightboxModal);
-
-    /*********** Launching and closing lightbox ***********/
-//}
+/*********** Go to previous and next media ***********/
