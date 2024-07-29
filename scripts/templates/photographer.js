51a22f97b3eb5f5dae9792753edf99a9ab7f9a55
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-07-29 20:30:28 +0200

    New display for the profile header

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 export function photographerTemplate(photographer) {
-    const { name, portrait, city, country, tagline } = photographer;
+    const { name, portrait, city, country, tagline, price } = photographer;
 
     const profile = `assets/photographers/${portrait}`;
 
@@ -7,6 +7,8 @@ export function photographerTemplate(photographer) {
     function getUserCardProfile() {
         const photographerHeader = document.querySelector('.photographer_header');
 
+        const photographerHeaderContent1 = document.createElement('div');
+        photographerHeaderContent1.classList.add('photographer_header_content');
         const photographerProfile = document.createElement('div');
         photographerProfile.classList.add('profile');
         photographerProfile.tabIndex = "0";
@@ -24,22 +26,58 @@ export function photographerTemplate(photographer) {
         photographerTaglineElement.classList.add('tagline');
         photographerTaglineElement.textContent = tagline;
 
+        const photographerHeaderContent2 = document.createElement('div');
+        photographerHeaderContent2.classList.add('photographer_header_content');
         const btnContact = document.querySelector('.contact_button');
 
+        const photographerHeaderContent3 = document.createElement('div');
+        photographerHeaderContent3.classList.add('photographer_header_content');
         const photographerImg = document.createElement('img');
         photographerImg.setAttribute('src', profile);
         photographerImg.alt = `${name}`;
         photographerImg.tabIndex = "0";
 
-        photographerHeader.appendChild(photographerProfile);
+        photographerHeader.appendChild(photographerHeaderContent1);
+        photographerHeader.appendChild(photographerHeaderContent2);
+        photographerHeader.appendChild(photographerHeaderContent3);
+        photographerHeaderContent1.appendChild(photographerProfile);
         photographerProfile.appendChild(h1);
         photographerProfile.appendChild(photographerProfileText);
         photographerProfileText.appendChild(h2);
         photographerProfileText.appendChild(photographerTaglineElement);
-        photographerHeader.appendChild(btnContact);
-        photographerHeader.appendChild(photographerImg);
+        photographerHeaderContent2.appendChild(btnContact);
+        photographerHeaderContent3.appendChild(photographerImg);
 
         return (photographerHeader);
+    }
+
+    // Fonction pour cliquer sur le coeur, mais je comprends pas tout D:
+    // Après il faudra aussi ajouter la touche entrée pour l'accessibilité
+    /*
+    function handleLikesButton() {
+        const that = this
+        
+        this.$wrapper
+            .querySelector('.like-on-click')
+            .addEventListener('click', function() {
+                if (this.classList.contains('wished')) {
+                    this.classList.remove('wished')
+                    that.TotalLikesSubject.fire('DEC')
+                } else {
+                    this.classList.add('wished')
+                    that.TotalLikesSubject.fire('INC')
+                }
+            })
+    }
+    */
+
+    // Maintenant il faut l'appeler dans le fichier /pages/photographer.js ... Mais où ?
+    function getPriceTotalLikesBox() {
+        const priceBox = document.querySelector('.price-box');
+        
+        priceBox.innerHTML = `${price}` + '€/jour';
+
+        return priceBox;
     }
 
     // To get the respective medium for each photographer. It contains the img and video functions
@@ -51,6 +89,8 @@ export function photographerTemplate(photographer) {
         if (medium.video) {
             return getUserVideo(path,medium)
         } 
+
+        //handleLikesButton()
         return getUserImage(path, medium)
     }
 
@@ -87,15 +127,6 @@ export function photographerTemplate(photographer) {
         card.querySelector('.medium-card_likes').textContent = `${likes}`;
 
         return card;
-    }
-
-    function getPriceTotalLikesBox() {
-        const { price } = price;
-        const priceBox = document.querySelector('.price-box');
-        
-        priceBox.textContent = `${price}` + '€/jour';
-
-        return priceBox;
     }
    
     return { getUserCardProfile, getUserMedium, getPriceTotalLikesBox}
