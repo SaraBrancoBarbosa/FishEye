c290f67b6366b80a32e80807769e09a2eb586dea
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-07-30 17:15:11 +0200

    Opens lightbox by pressing the enter key

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,7 +3,7 @@ import { launchLightboxModal } from "../utils/lightbox.js"
 
 async function getPhotographer() {
     
-    // Cherche dans la barre URL
+    // Cherche l'ID des photographes dans la barre URL
     const id = new URLSearchParams(window.location.search).get('id')
     // Récupération des données depuis le fichier JSON   
     const database = await fetch('data/database.json')
@@ -20,28 +20,39 @@ async function getPhotographer() {
     )
 }
 
-export async function displayData(photographer, media) {
+async function displayData(photographer, media) {
     const photographerMedias = document.querySelector('.photographer_medias');
 
-        const photographerModel = photographerTemplate(photographer);
-        
-        photographerModel.getUserCardProfile();
-        media.forEach(medium => {
-            const card = photographerModel.getUserMedium(photographer, medium)
-            
-            let src = null; 
-            if (medium.video) {
-                src = card.querySelector('.medium-card_video')
-              } else {
-               src = card.querySelector('.medium-card_img')
-              }
-
-            src.onclick = () => {
-                launchLightboxModal()
-            }
-            photographerMedias.appendChild(card)
-        })
+    const photographerModel = photographerTemplate(photographer);
     
+    photographerModel.getUserCardProfile();
+
+    const path = '/assets/photographers/'+(photographer.name.split(' ')[0].replaceAll('-', ' '));
+
+    media.forEach((medium, index) => {
+        const card = photographerModel.getUserMedium(photographer, medium)
+        
+        let src = null; 
+        if (medium.video) {
+            src = card.querySelector('.medium-card_video')
+            } else {
+            src = card.querySelector('.medium-card_img')
+            }
+
+        // Lightbox  
+        src.onclick = () => {
+            launchLightboxModal(path, media, index)
+        }
+        // Opens Lightbox by pressing the enter key
+        src.onkeydown = function(e){
+            if(e.keyCode == 13){
+                launchLightboxModal(path, media, index)
+            }
+         };
+        
+
+        photographerMedias.appendChild(card)
+    })
 }
 
 export async function initPhotographer() {
