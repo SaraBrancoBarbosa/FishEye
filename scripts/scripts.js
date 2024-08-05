aa0ee3357703c333e877a465eebc287b146c0262
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-08-05 12:16:14 +0200

    Delete a useless js file

--- scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-import { init } from "./pages/index.js";
-//import { initPhotographer } from "./pages/photographer.js";
-
-// le window.onload prépare le script et quand la page est chargée, il est prêt
-window.onload = () => {
-    init()
-    //initPhotographer()
-}
-
