c2e25b7ef45b7ba8ad94730ad050f3f448c129a2
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-08-06 18:05:10 +0200

    Total likes - first steps with working persistence but not the likes count

---
+++ scripts/utils/totalLikes.js
@@ -0,0 +1,50 @@
+export function totalLikesMedia() {    
+
+    // Fonction pour mettre à jour l'affichage des likes
+    function updateLikeDisplay(pContainer) {
+        const likeCount = pContainer.querySelector(".medium-card_likes");
+
+        // Récupère le nombre de likes du localStorage
+        const likes = localStorage.getItem(pContainer.dataset.id);
+        // Met à jour l'affichage
+        likeCount.textContent = likes;
+    }
+        
+    // Initialisation des likes à partir du localStorage
+    document.querySelectorAll(".card").forEach(pContainer => {
+        // Met à jour l'affichage pour chaque image
+        updateLikeDisplay(pContainer);
+
+        const likeIcon = pContainer.querySelector(".like_icon");
+        likeIcon.addEventListener("click", () => {
+            const currentLikes = parseInt(localStorage.getItem(pContainer.dataset.id));
+            let newLikes;
+
+            // Vérifie si le bouton est déjà ''liké''
+            if (likeIcon.classList.contains("liked")) {
+                // Si oui, on enlève le like
+                newLikes = currentLikes - 1;
+                likeIcon.classList.remove("liked");
+            } else {
+                // Sinon, on ajoute un like
+                newLikes = currentLikes + 1;
+                likeIcon.classList.add("liked");
+            }
+
+            // Met à jour le localStorage
+            localStorage.setItem(pContainer.dataset.id, newLikes);
+            // Met à jour l'affichage
+            updateLikeDisplay(pContainer);
+        });
+    });
+
+    // Pour la box en bas à droite
+    function getPriceTotalLikesBox() {
+        const priceBox = document.querySelector(".price-box");
+        const likeCounter = document.querySelector(".like-count");
+        
+        priceBox.innerHTML = `${price}` + "€/jour";
+
+        return priceBox;
+    }
+}
